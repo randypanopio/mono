06a4e9c70ebb21c477c09404ff7d1ed4c8e222c5
Randy Panopio <randypanopio@gmail.com>
2024-05-14 21:19:46 -0700

    sync ALL changes

---
+++ 225/Labs/Lab0/hello.cpp
@@ -0,0 +1,19 @@
+/*
+ * hello.cpp
+ * 
+ * Description: Solution to the good old "Hello world! problem.
+ *
+ * Author: Randy Panopio
+ * Last modified on: May 2024
+ */
+ 
+#include <iostream>
+using namespace std;
+
+int main (void) {
+    int someInteger = 0;
+    cin >> someInteger;
+    cout << endl << "The value of someInteger is: " << someInteger << endl;
+    cout << "Hello world!";
+    return 0;
+}
