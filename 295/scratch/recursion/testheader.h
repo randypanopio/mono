72401a354bedc911a0aabfe393ff62b0e3f913d1
Randy Panopio <randypanopio@gmail.com>
2024-06-10 23:18:27 -0700

    finish lab5

---
+++ 295/scratch/recursion/testheader.h
@@ -0,0 +1,8 @@
+#ifndef LAB4_H
+#define LAB4_H
+
+#include <stdint.h>
+
+uint64_t recurse(uint64_t n);
+
+#endif
---
+++ 295/scratch/recursion/tests.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "testheader.h"
+
+int main() {
+    printf("Testing Assembly Functions \n\n");
+    printf("recurse(2) %lu\n", recurse(5));
+
+}
