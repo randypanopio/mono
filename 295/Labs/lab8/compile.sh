96994219583aa15e0f19d22d3a55351fa7f6a3f6
Randy Panopio <randypanopio@gmail.com>
2024-07-01 19:43:33 -0700

    sync ALL changes

---
+++ 295/Labs/lab8/compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "setting virtual memory to 4gb"
+ulimit -v 4194304  # limit to 4GB virtual memory
+sudo gcc -Wall -Wpedantic -std=c17 -march=haswell -O3 array-2d.c
+./a.out 5120000
